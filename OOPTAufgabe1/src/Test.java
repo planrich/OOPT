12eeff85f12d7da66e1424be4fe66fdd74715a90
Richard Plangger <Richard.Plangger@gmx.net>
2011-10-12 16:15:47 +0200

    added alot of data

--- OOPTAufgabe1/src/Test.java
+++ OOPTAufgabe1/src/Test.java
@@ -11,16 +11,32 @@ public class Test {
 		
 		System.out.println("-Anlegen der Lehrveranstaltungen");
 		
-		Course oopt = CourseManager.createNewCourse("123456", "Objektorientierten Programmiertechniken");
-		oopt.enrole(new Student(1025637, "Richard Plangger"));
-		oopt.enrole(new Student(1025990, "Simon Zuend"));
-		oopt.enrole(new Student(1025991, "Klaus Nigsch"));
+		CourseManager.createNewStudent(new Student("1025637", "Richard Plangger"));
+		CourseManager.createNewStudent(new Student("1025990", "Simon Zuend"));
+		CourseManager.createNewStudent(new Student("1025991", "Klaus Nigsch"));
+		CourseManager.createNewStudent(new Student("1046344", "Max Mustermann"));
+		CourseManager.createNewStudent(new Student("0232454", "Simon Stammler"));
+		CourseManager.createNewStudent(new Student("1034523", "Lukas Fleischhacker"));
+		CourseManager.createNewStudent(new Student("1025992", "Gabriel"));
+		CourseManager.createNewStudent(new Student("0825993", "Morgan Freeman"));
+		CourseManager.createNewStudent(new Student("0025994", "John Rambo"));
+		CourseManager.createNewStudent(new Student("0025995", "Arnold Schwarzenegger"));
+		CourseManager.createNewStudent(new Student("1025996", "Albert Einstein"));
+		
+		CourseManager.createNewCourse("123.456", "Objektorientierte Programmiertechniken");
+		CourseManager.createNewCourse("123.451", "Funktionale Programmierung");
+		CourseManager.createNewCourse("133.412", "Statistik und Wahrscheinlichkeitsrechnung");
+		CourseManager.createNewCourse("133.452", "Logikorientiere Programmierung");
+		CourseManager.createNewCourse("173.432", "Mathematik 2");
+		CourseManager.createNewCourse("133.451", "Daten- und Informatikrecht");
+		
+		CourseManager.enrole("","");
 		
 		System.out.println("-Liste aller LVAs:");
 		List<Course> courses = null;
 		//list all course informations
 		for (Course c : courses) {
-			System.out.println(c); 
+			System.out.println(String.format("",)); 
 		}
 		
 		//list all students that are enroled into any course
