1c9d841239ac6c585fc8676c08eaf94b00ce427a
Richard Plangger <Richard.Plangger@gmx.net>
2011-10-12 14:18:00 +0200

    how I would do it

---
+++ OOPTAufgabe1/src/Student.java
@@ -0,0 +1,18 @@
+/**
+ * A student at any university
+ * 
+ * @author rich
+ * @date 12.10.2011
+ */
+
+public class Student {
+
+	private final int matrikelNumber;
+	private final String name;
+	
+	public Student(int matrikelNumber, String name) {
+		this.matrikelNumber = matrikelNumber;
+		this.name = name;
+	}
+
+}
--- OOPTAufgabe1/src/Test.java
+++ OOPTAufgabe1/src/Test.java
@@ -3,5 +3,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("Hello OOPT Challenge 1");
+		
+		LVA lva = new LVA(198762, "Objektorientierte Programmiertechniken");
+		
+		lva.enrole(new Student(1025637, "Richard Plangger"));
+		
+		//do some other checks
 	}
 }
