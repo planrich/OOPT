2299e3a6ae1221b3b7dceb4746e013961a174868
Klaus Nigsch <nigsch_petermorphose@gmx.at>
2011-10-12 16:20:49 +0200

    Added enrole method

--- OOPTAufgabe1/src/oopt/round1/CourseManager.java
+++ OOPTAufgabe1/src/oopt/round1/CourseManager.java
@@ -1,12 +1,15 @@
 package oopt.round1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class CourseManager {
 
 	private static final List<Course> courses = new ArrayList<Course>();
 	private static final List<Student> students = new ArrayList<Student>();
+	private static final Map<String, List<String>> courseStudents = new HashMap<String, List<String>>();
 	
 	public static Course createNewCourse(String lvaIdentifier, String name) {
 		Course course = new Course(lvaIdentifier, name);
@@ -26,5 +29,14 @@ public class CourseManager {
 		
 		students.add(student);
 		return student;
+	}
+	
+	public static void enrole(String lvaIdentifier, String matrikelNumber) {
+		List<String> studentsInCourse = courseStudents.get(lvaIdentifier);
+		if (studentsInCourse == null) {
+			studentsInCourse = new ArrayList<String>();
+			courseStudents.put(lvaIdentifier, studentsInCourse);
+		}
+		studentsInCourse.add(matrikelNumber);
 	}
 }
