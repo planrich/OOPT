a64755386a4b944e8e082ecce5643bb54047aee4
Simon Zuend <simon.zuend@gmx.net>
2011-10-22 15:37:26 +0200

    Added registerStudent and unregisterStudent

--- OOPTAufgabe2/src/oopt/round2/Registerable.java
+++ OOPTAufgabe2/src/oopt/round2/Registerable.java
@@ -7,10 +7,35 @@ import java.util.List;
 public abstract class Registerable {
 	
 	protected final List<Restriction> restrictions = new ArrayList<Restriction>();
+	protected final List<Student> students = new ArrayList<Student>();
 	
 	protected Date earlyEnrol = new Date();
 	protected Date lateEnrol = new Date();
 	protected Date lateUnenrol = new Date();
+	
+	public boolean registerStudent(Student student) {
+		Date now = new Date();
+		if (now.before(earlyEnrol) || now.after(lateEnrol)) {
+			return false;
+		}
+		
+		// TODO: check here for restriction
+		
+		return students.add(student);
+	}
+	
+	public boolean unregisterStudent(Student student) {
+		if (!students.contains(student)) {
+			return false;
+		}
+		
+		Date now = new Date();
+		if (now.after(lateUnenrol)) {
+			return false;
+		}
+		
+		return students.remove(student);
+	}
 	
 	public Date getEarlyEnrol() {
 		return earlyEnrol;
