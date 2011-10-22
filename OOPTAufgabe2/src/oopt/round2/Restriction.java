655d8bff0a03196e66babd12b71d43ac9579c324
Klaus Nigsch <nigsch_petermorphose@gmx.at>
2011-10-22 14:53:17 +0200

    Started Restrictions class and created empty Achievement class

--- OOPTAufgabe2/src/oopt/round2/Restriction.java
+++ OOPTAufgabe2/src/oopt/round2/Restriction.java
@@ -1,5 +1,16 @@
 package oopt.round2;
 
+import java.util.List;
+import java.util.ArrayList;
+// abfragen, ob Student Restrictions erfüllt
 public class Restriction {
+	
+	public List<Achievement> achievements = new ArrayList<Achievement>();
 
+	public Restriction() {	
+	}
+	
+	public void addAchievement(Achievement achievement) {
+		achievements.add(achievement);
+	}
 }
