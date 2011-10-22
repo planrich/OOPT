30badfd33b58ddf080c74d5cb5a95d483119881b
Richard Plangger <Richard.Plangger@gmx.net>
2011-10-22 17:34:28 +0200

    ...

--- OOPTAufgabe2/src/Test.java
+++ OOPTAufgabe2/src/Test.java
@@ -1,10 +1,12 @@
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import oopt.round2.Achievement;
 import oopt.round2.Course;
 import oopt.round2.Exam;
 import oopt.round2.Registerable;
+import oopt.round2.Restriction;
 import oopt.round2.Student;
 import oopt.round2.UniversityService;
 
@@ -12,8 +14,6 @@ import oopt.round2.UniversityService;
 public class Test {
 
 	public static void main(String[] args) {
-		
-		System.out.println("###Anlegen der Lehrveranstaltungen");
 		
 		UniversityService universityService = new UniversityService();
 		
@@ -27,104 +27,108 @@ public class Test {
 		universityService.createNewStudent("1025992", "Erzengel Gabriel");
 		universityService.createNewStudent("0825993", "John Rambo");
 		universityService.createNewStudent("0025994", "Morgan Freeman");
-		universityService.createNewStudent("0025995", "Arnold Schwarzenegger");
-		universityService.createNewStudent("1025996", "Albert Einstein");
-		
-		//courses
-		Course course = universityService.createNewCourse("123.456", "Objektorientierte Programmiertechniken");
-		course.setLateEnrol(new Date(System.currentTimeMillis() + (10 * 60 * 60 * 24) * 100)); //in 10 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (11 * 60 * 60 * 24) * 100)); //in 11 days
-		course = universityService.createNewCourse("123.451", "Funktionale Programmierung");
-		course.setLateEnrol(new Date(System.currentTimeMillis() + (1 * 60 * 60 * 24) * 100)); //in 1 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (10 * 60 * 60 * 24) * 100)); //in 10 days
-		course = universityService.createNewCourse("133.412", "Statistik und Wahrscheinlichkeitsrechnung");
-		course.setLateEnrol(new Date(System.currentTimeMillis() + (2 * 60 * 60 * 24) * 100)); //in 2 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (5 * 60 * 60 * 24) * 100)); //in 5 days
-		course = universityService.createNewCourse("133.452", "Logikorientiere Programmierung");
-		course.setLateEnrol(new Date(System.currentTimeMillis() + (7 * 60 * 60 * 24) * 100)); //in 7 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (10 * 60 * 60 * 24) * 100)); //in 10 days
-		course = universityService.createNewCourse("133.451", "Bodybuilding");
-		course.setLateEnrol(new Date(System.currentTimeMillis() + (10 * 60 * 60 * 24) * 100)); //in 10 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (10 * 60 * 60 * 24) * 100)); //in 10 days
+		Student arnold = universityService.createNewStudent("0025995", "Arnold Schwarzenegger");
+		Student albert_einstein = universityService.createNewStudent("1025996", "Albert Einstein");
 
-		course = universityService.createNewCourse("173.432", "Mathematik 2");
-		course.setLateEnrol(new Date(System.currentTimeMillis() - (3 * 60 * 60 * 24) * 100)); //before 3 days
-		course.setEarlyEnrol(new Date(System.currentTimeMillis() - (4 * 60 * 60 * 24) * 100)); //before 4 days
-		course.setLateUnenrol(new Date(System.currentTimeMillis() + (2 * 60 * 60 * 24) * 100)); //in 2 days
+
+		//mathe 1
+		Course mathe1 = universityService.createNewCourse("174.431", "Mathematik 1");
+		mathe1.setMaxStudents(Integer.MAX_VALUE);
+		mathe1.setLateEnrol(new Date(System.currentTimeMillis() + (3 * 60 * 60 * 24) * 100)); //in 3 days
+		mathe1.setEarlyEnrol(new Date(System.currentTimeMillis() - (4 * 60 * 60 * 24) * 100)); //before 4 days
+		mathe1.setLateUnenrol(new Date(System.currentTimeMillis() + (6 * 60 * 60 * 24) * 100)); //in 6 days
 		
-		course = universityService.createNewCourse("174.431", "Mathematik 1");
+		//mathe 2
+		Course mathe2 = universityService.createNewCourse("173.432", "Mathematik 2");
+		mathe1.setMaxStudents(Integer.MAX_VALUE);
+		mathe2.setLateEnrol(new Date(System.currentTimeMillis() + (3 * 60 * 60 * 24) * 100)); //in 3 days
+		mathe2.setEarlyEnrol(new Date(System.currentTimeMillis() - (4 * 60 * 60 * 24) * 100)); //before 4 days
+		mathe2.setLateUnenrol(new Date(System.currentTimeMillis() + (6 * 60 * 60 * 24) * 100)); //in 6 days
+		
 		Registerable mathe1_exam = new Exam("EI 7", new Date(), new Date());
-		//course.
+		mathe1_exam.setMaxStudents(10);
+		mathe1_exam.setLateEnrol(new Date(System.currentTimeMillis() + (3 * 60 * 60 * 24) * 100)); //in 3 days
+		mathe1_exam.setEarlyEnrol(new Date(System.currentTimeMillis() - (4 * 60 * 60 * 24) * 100)); //before 4 days
+		mathe1_exam.setLateUnenrol(new Date(System.currentTimeMillis() + (6 * 60 * 60 * 24) * 100)); //in 6 days
 		
-		
-		
-		//achievments
-		Achievement steop = new Achievement("");
-		Achievement steg = new Achievement("");
-		
-		
+		mathe1.add(mathe1_exam);
+		Achievement mathe1_achieve = new Achievement(mathe1_exam, 6);
+		mathe1_exam.setAchievement(mathe1_achieve);
+
+		Restriction r = new Restriction("Examination Mathematik 1");
+		r.addAchievement(mathe1_achieve);
+		mathe2.getRestrictions().add(r);
 		
 		//enrolements
-		System.out.println("###Checking enrolements in the valid enrolement timespan:");
-		System.out.print(String.format("%s is trying to enrole to %s...","0025995","133.451"));
-		if (universityService.enrole("133.451","0025995")) {
-			System.out.println("great success!");
+		System.out.println("###Checking enrolements:");
+		System.out.print("Registering Albert Einstein to Mathematik 1...");
+		if (mathe1.registerStudent(albert_einstein)) {
+			System.out.println("great success");
 		} else {
+			System.out.println("fails");
+		}
+		
+		System.out.print("Registering Albert Einstein to Mathematik 1 Examination...");
+		if (mathe1_exam.registerStudent(albert_einstein)) {
+			System.out.println("great success");
+		} else {
+			System.out.println("fails");
+		}
+		
+		System.out.print("Albert Einstein makes the Exam for Mathematik 1...");
+		if (universityService.pass(albert_einstein, mathe1_exam)) {
+			System.out.println("passes");
+		} else {
+			System.out.println("fails");
+		}
+		
+		System.out.print("Albert Einstein is now ready for Mathematik 2...");
+		if (mathe2.registerStudent(albert_einstein)) {
+			System.out.println("yes");
+		} else {
+			System.out.println("no");
+		}
+		
+		System.out.print("Arnold now tries to enrole to Mathematik 2 without having made Mathematik 1 Examination...");
+		if (mathe2.registerStudent(arnold)) {
+			System.out.println("greate success");
+		} else {
+			System.out.println("fails so bad");
+		}
+		
+		//delete a course
+		System.out.println("###Checking deleting feature");
+		System.out.println("Creating course Body Building");
+		universityService.createNewCourse("123.333", "Body Building");
+		System.out.println("Deleting course Body Building");
+		universityService.deleteCourse("123.333");
+		System.out.print("Arnold wants to enrole to Body Building...");
+		try {
+			universityService.enrole("123.333", "0025995");
+			System.out.println("great success");
+		} catch (NoSuchElementException e) {
 			System.out.println("failed");
 		}
-		System.out.print(String.format("%s is trying _again_ to enrole to %s...","0025995","133.451"));
-		if (universityService.enrole("133.451","0025995")) {
-			System.out.println("great success!");
-		} else {
-			System.out.println("failed");
-		}
-		System.out.print(String.format("%s is trying to unenrole from %s...","0025995","133.451"));
-		if (universityService.unenrole("133.451","0025995")) {
-			System.out.println("great success!");
-		} else {
+		System.out.println("Poor Arnold -> enabling course again");
+		universityService.enableCourse("123.333");
+		System.out.print("Arnold wants to enrole _again_ to Body Building...");
+		try {
+			universityService.enrole("123.333", "0025995");
+			System.out.println("great success");
+		} catch (NoSuchElementException e) {
 			System.out.println("failed");
 		}
 		
-		universityService.enrole("173.432","1025991");
-		universityService.enrole("123.456","1025637");
-		universityService.enrole("123.456","1025990");
-		universityService.enrole("123.456","1034523");
-		universityService.enrole("123.456","0025995");
-		universityService.enrole("123.456","0025995");
-		universityService.enrole("133.412","0025995");
-		universityService.enrole("123.456","0025995");
-		universityService.enrole("123.456","0025994");
-		universityService.enrole("133.451","0825993");
-		universityService.enrole("123.451","1025996");
+		//cancel
+		System.out.println("###Checking cancel feature");
+		universityService.enrole("123.333", "1025996");
+		universityService.enrole("123.333", "1025637");
+		universityService.enrole("123.333", "1025990");
+		universityService.enrole("123.333", "1034523");
+		System.out.println("Taking course Body Building and canceling it.");
+		universityService.cancelCourse("123.333");
 		
-		System.out.println("###Liste aller LVAs:");
-		List<Course> courses = universityService.getCourses();
-		//list all course informations
-		for (Course c : courses) {
-			
-			int size = universityService.studentCountIn(c.getLvaIdentifier());
-			
-			System.out.println(String.format(
-					"%s : %s" +
-					"\n\tAnmeldungbegin: %s" +
-					"\n\tAnmeldungsende: %s" +
-					"\n\tAbmeldungsende: %s" +
-					"\n\tAnzahl der angemeldeten Studenten: %d",
-					c.getLvaIdentifier(), 
-					c.getName(), 
-					c.getEarlyEnrol(),
-					c.getLateEnrol(),
-					c.getLateUnenrol(),
-					size)); 
-		}
 		
-		System.out.println("###Liste aller Studenten:");
-		//list all students that are enroled into any course
-		List<Student> enroledStudents = universityService.getStudents();
-		for (Student student : enroledStudents) {
-			System.out.println(String.format("%s %s",
-					student.getMatrikelNumber(),
-					student.getName()));
-		}
+		
 	}
 }
