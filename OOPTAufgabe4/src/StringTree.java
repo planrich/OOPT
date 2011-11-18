d2d443b290a7c1b03e6d1c29da3eb542cefcb5f0
Simon Zuend <simon.zuend@gmx.net>
2011-11-18 10:41:51 +0100

    Added missing classes

--- OOPTAufgabe4/src/StringTree.java
+++ OOPTAufgabe4/src/StringTree.java
@@ -5,7 +5,7 @@ public interface StringTree {
 	static final String NODE_LEFT = "left";
 	static final String NODE_RIGHT = "right";
 	
-	public boolean contains(String node); // node not null
-	public String search(String node); // node not null
-	public void add(String node); // node not null
+	boolean contains(String node); // node not null
+	String search(String node); // node not null
+	void add(String node); // node not null
 }
