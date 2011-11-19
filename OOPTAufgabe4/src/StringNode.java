6800160ff454aeabd438c991facc9bc4bf123240
Richard Plangger <Richard.Plangger@gmx.net>
2011-11-19 12:27:51 +0100

    forgot string node also added intnode and inttree

---
+++ OOPTAufgabe4/src/StringNode.java
@@ -0,0 +1,28 @@
+
+public class StringNode extends Node {
+	
+	private String label;
+	
+	public StringNode(String label) {
+		this.label = label;
+		left = null;
+		right = null;
+	}
+
+	@Override
+	public Object getLabel() {
+		return label;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (obj == this) {
+			return true;
+		}
+		if (obj instanceof StringNode) {
+			StringNode node = (StringNode) obj;
+			return label.equals(node.getLabel());
+		}
+		return false;
+	}
+}
