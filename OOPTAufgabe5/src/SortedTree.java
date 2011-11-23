5184485af9a5c68e74086f1f93c7b7bbf9244927
Simon Zuend <simon.zuend@gmx.net>
2011-11-23 15:19:21 +0100

    Added SortedTree sceleton.

---
+++ OOPTAufgabe5/src/SortedTree.java
@@ -0,0 +1,14 @@
+
+public abstract class SortedTree<E extends Comparable<E>> extends Tree<E> {
+
+	@Override
+	public void add(E data) {
+		// TODO Auto-generated method stub	
+	}
+
+	@Override
+	public Iter<Boolean> search(E data) {
+		// TODO Auto-generated method stub
+		return super.search(data);
+	}
+}
