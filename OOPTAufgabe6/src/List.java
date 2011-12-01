07230ee72bab34cc873ca7e9577aafbf8128a378
Simon Zuend <simon.zuend@gmx.net>
2011-12-01 13:51:27 +0100

    Added List.java from the previous lesson.

---
+++ OOPTAufgabe6/src/List.java
@@ -0,0 +1,108 @@
+
+
+public class List<E> {
+
+	private ListElement headElement = null;
+	private ListElement tailElement = null;
+	
+	public void append(E element) {
+		ListElement e = new ListElement(element);
+		if (headElement == null) {
+			headElement = e;
+			tailElement = e;
+		} else {
+			tailElement.setNext(e);
+			e.setPrevious(tailElement);
+			tailElement = e;
+		}
+	}
+	
+	public Iterator<E> iterator() {
+		return new ListIter(headElement);
+	}
+	
+	public interface Iterator<E> {
+		E next();
+		E previous();
+		boolean hasNext();
+		boolean hasPrevious();
+	}
+	
+	private class ListIter implements Iterator<E> {
+
+		private ListElement next;
+		private ListElement previous;
+		private ListElement current;
+		
+		public ListIter(ListElement head) {
+			next = head;
+			current = null;
+			previous = null;
+		}
+		
+		@Override
+		public E next() {
+			if (next == null) {
+				return null;
+			}
+			current = next;
+			next = next.getNext();
+			previous = current.getPrevious();
+			return current.getElement();
+		}
+
+		@Override
+		public E previous() {
+			if (previous == null) {
+				return null;
+			}
+			current = previous;
+			next = current.getNext();
+			previous = current.getPrevious();
+			return current.getElement();
+		}
+
+		@Override
+		public boolean hasNext() {
+			return next != null;
+		}
+
+		@Override
+		public boolean hasPrevious() {
+			return previous != null;
+		}
+	}
+	
+	private class ListElement {
+		
+		private E element;
+		private ListElement next;
+		private ListElement previous;
+		
+		public ListElement(E element) {
+			this.element = element;
+			this.next = null;
+			this.previous = null;
+		}
+		
+		public E getElement() {
+			return element;
+		}
+		
+		public ListElement getNext() {
+			return next;
+		}
+		
+		public void setNext(ListElement next) {
+			this.next = next;
+		}
+
+		public ListElement getPrevious() {
+			return previous;
+		}
+
+		public void setPrevious(ListElement previous) {
+			this.previous = previous;
+		}
+	}
+}
