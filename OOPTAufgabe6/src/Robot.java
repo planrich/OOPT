b962f50c2aaee110b666b4d686d51a8ea64745d6
Simon Zuend <simon.zuend@gmx.net>
2011-12-01 13:46:00 +0100

    Tried a first role concept for the robots.

--- OOPTAufgabe6/src/Robot.java
+++ OOPTAufgabe6/src/Robot.java
@@ -3,6 +3,7 @@ public abstract class Robot {
 
 	private int number;
 	private int hoursOperating;
+	private Role role;
 	
 	public Robot(int number) {					// must be unique
 		this.number = number;
@@ -19,5 +20,49 @@ public abstract class Robot {
 	
 	public int getNumber() {
 		return number;
+	}
+	
+	public void toWeldingRobot(int temperature) {
+		role = new WeldingRobot(temperature);
+	}
+	
+	public void toVarnishRobot(double capacity) {
+		role = new VarnishRobot(capacity);
+	}
+	
+	private abstract class Role {
+		public abstract double getData();
+	}
+	
+	private class WeldingRobot extends Role {
+		private int temperature;
+		
+		public WeldingRobot(int temperature) {
+			this.temperature = temperature;
+		}
+		
+		public int getTemperature() {
+			return temperature;
+		}
+		
+		public double getData() {
+			return temperature;
+		}
+	}
+	
+	private class VarnishRobot extends Role {
+		private double capacity;
+		
+		public VarnishRobot(double capacity) {
+			this.capacity = capacity;
+		}
+		
+		public double getCapacity() {
+			return capacity;
+		}
+
+		public double getData() {
+			return capacity;
+		}
 	}
 }
