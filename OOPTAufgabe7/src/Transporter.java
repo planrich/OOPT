f803bda59cd99a4202bc0ba0e7d0e3a9b8b061d6
Richard Plangger <Richard.Plangger@gmx.net>
2011-12-07 16:41:17 +0100

    minor mod transporter but far away from complete

--- OOPTAufgabe7/src/Transporter.java
+++ OOPTAufgabe7/src/Transporter.java
@@ -1,4 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Transporter {
-
+	private final List<Animal> animals = new ArrayList<Animal>();
+	private Transporter followingTransporter;
+	
+	public void setFollowingTransporter(Transporter followingTransporter) {
+		this.followingTransporter = followingTransporter;
+	}
+	
+	/**
+	 * This transporter must not be a pulling vehicle
+	 * @return all {@link Animal}s from this transporter and all following ones.
+	 */
+	public List<Animal> list() {
+		List<Animal> list = new ArrayList<Animal>();
+		
+		list.addAll(animals);
+		
+		list.addAll(followingTransporter.list());
+		
+		return list;
+	}
 }
